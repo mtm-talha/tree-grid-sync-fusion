0356bc0741914c60fd836335dc862a56437410f2
mtm-talha <talhamahmood2001@gmail.com>
2022-01-06 02:25:40 +0500

    implemented sync fusion treegrid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TreeGridModule, InfiniteScrollService, PageService, ColumnChooserService, ToolbarService } from '@syncfusion/ej2-angular-treegrid';
+import { SortService, FilterService } from '@syncfusion/ej2-angular-treegrid';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeGridModule,
   ],
-  providers: [],
+  providers: [
+    ToolbarService,
+    PageService,
+    ColumnChooserService,
+    InfiniteScrollService,
+    SortService,
+    FilterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
