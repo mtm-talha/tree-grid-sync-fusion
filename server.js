f92e10278b51aa8f0d7c3b259fa0555ebee6403d
mtm-talha <talhamahmood2001@gmail.com>
2022-01-07 00:58:11 +0500

    fix deploy failed issue by exceeding budget

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression')
+
 const app = express();
 app.use(express.static(__dirname + '/dist/tree-grid-sync-fusion'));
+app.use(compression())
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/tree-grid-sync-fusion/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
