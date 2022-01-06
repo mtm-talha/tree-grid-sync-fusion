20ef6f22f0c5ac8655fb013893f29069e50d0196
Hamza Khan <mhamzak3@gmail.com>
2022-01-06 20:56:27 +0500

    Edit or add Modal Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Inject } from '@angular/core';
 import { dataSource, virtualData } from './datasource';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { BeforeOpenCloseEventArgs } from '@syncfusion/ej2-inputs';
+import { MenuEventArgs } from '@syncfusion/ej2-navigations';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,19 +16,22 @@ import { dataSource, virtualData } from './datasource';
 })
 export class AppComponent implements OnInit {
   title = 'treeGrid';
+  animal: string | undefined;
+  name: string | undefined;
 
   public vData: Object[] = [];
   public pageSettings: Object | undefined;
   public toolbar: string[] = [];
   public contextMenuItems: Object[] = [];
 
+  constructor(public dialog: MatDialog) {}
   ngOnInit(): void {
       dataSource();
       this.vData = virtualData;
       this.pageSettings= { pageSize: 50 };
       this.toolbar = ['ColumnChooser'];
       this.contextMenuItems =  [
-        {text: 'Add Column', target: '.e-headercontent', id: ''},
+        {text: 'Add Column', target: '.e-headercontent', id: 'addHeaderColoumn'},
         {text: 'Edit Column', target: '.e-headercontent', id: ''},
         {text: 'Delete Column', target: '.e-headercontent', id: ''},
         {text: 'Choose Column', target: '.e-headercontent', id: ''},
@@ -40,6 +51,42 @@ export class AppComponent implements OnInit {
     ];
 }
 
-contextMenuOpen(arg?: any): void {}
+contextMenuOpen(arg?: BeforeOpenCloseEventArgs): void {}
 
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+  contextMenuClick (args: MenuEventArgs): void {
+    if (args.item.id === 'addHeaderColoumn') {
+      this.openDialog();
+    } 
+}
+
+}
+
+@Component({
+  selector: 'DialogComponent',
+  templateUrl: './dialog.component.html',
+
+})
+export class DialogComponent {
+  public fontColor: string = '';
+  public backGroundColor: string = '';
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent , DialogComponent } from './app.component';
 import { TreeGridModule, InfiniteScrollService, PageService, ColumnChooserService, ToolbarService } from '@syncfusion/ej2-angular-treegrid';
 import { SortService, ResizeService, ExcelExportService, PdfExportService, ContextMenuService } from '@syncfusion/ej2-angular-treegrid';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { ColorPickerModule } from 'ngx-color-picker';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
     TreeGridModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    ColorPickerModule
   ],
   providers: [
     ToolbarService,
@@ -19,9 +31,9 @@ import { SortService, ResizeService, ExcelExportService, PdfExportService, Conte
     ColumnChooserService,
     InfiniteScrollService,
     SortService,
-    ResizeService, 
-    ExcelExportService, 
-    PdfExportService, 
+    ResizeService,
+    ExcelExportService,
+    PdfExportService,
     ContextMenuService,],
   bootstrap: [AppComponent]
 })
